0ebc491bfd46101a1944266885687df6e5ee50fd
David Salamanca <d.salamancaz@uniandes.edu.co>
2025-03-03 17:21:51 -0500

    Revert "Todo funcinando"
    This reverts commit 3f444b6e490a9a4426e7287b26f0685822d5ae17.

--- src/saludtech/modulos/anonimizador/dominio/entidades.py
+++ src/saludtech/modulos/anonimizador/dominio/entidades.py
@@ -7,7 +7,7 @@ import uuid
 class ImagenAnonimizada(Entidad):
     __tablename__ = "imagenes_anonimizadas"
 
-    _id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)  # Cambiado a _id
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     id_imagen_original = Column(UUID(as_uuid=True), nullable=False)
     url_imagen_original = Column(String, nullable=False)
     url_imagen_anonimizada = Column(String, nullable=False)
@@ -15,20 +15,14 @@ class ImagenAnonimizada(Entidad):
     fecha_creacion = Column(DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, id=None, id_imagen_original=None, url_imagen_original=None, url_imagen_anonimizada=None, estado_procesamiento="pendiente"):
-        super().__init__(id=id if id else uuid.uuid4()) #asegurar que se llame al constructor de la clase padre
+        #super().__init__(id if id else str(uuid.uuid4()))  # Solo genera un nuevo ID si no se proporciona uno
+        self.id = id if id else str(uuid.uuid4()) 
         self.id_imagen_original = id_imagen_original
         self.url_imagen_original = url_imagen_original
         self.url_imagen_anonimizada = url_imagen_anonimizada
         self.estado_procesamiento = estado_procesamiento
 
+
     def anonimizar_imagen(self):
         # Lógica de dominio para anonimizar la imagen
         self.estado_procesamiento = "procesado"
-
-    @property
-    def id(self):
-        return self._id
-
-    @id.setter
-    def id(self, id):
-        self._id = id
